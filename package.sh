9486642dad916565928a13df269ea16e4ff5bcaf
Arleth <pat.arleth@espn.com>
2018-08-17 07:43:47 -0400

    scripts to make icons and gen app package

--- package.sh
+++ package.sh
@@ -9,18 +9,8 @@ cd "$PACKAGE_DIR"
 target="${1:-macos}"
 
 if [ "$target" == "all" ] || [ "$target" == "macos" ]; then
-    if [ -e assets/icons/icon.icns ]; then
-        electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/icon.icns --prune=true --out=release-builds
-    else
-        echo assets/icons/icon.icns not found
-        electron-packager . --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds
-    fi
+    electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/icon.icns --prune=true --out=release-builds
 fi
 if [ "$target" == "all" ] || [ "$target" == "linux" ]; then
-    if [ -e assets/icons/png/icon_256.png ]; then
-        electron-packager . electron-tutorial-app --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/png/icon_256.png --prune=true --out=release-builds
-    else
-        echo assets/icons/icon_256.png not found
-        electron-packager . electron-tutorial-app --overwrite --asar=true --platform=linux --arch=x64 --prune=true --out=release-builds
-    fi
+    electron-packager . electron-tutorial-app --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/png/icon_256.png --prune=true --out=release-builds
 fi
