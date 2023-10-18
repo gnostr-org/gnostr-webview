66dba2d4cf6e864274cfe36888a0c923aabe51dc
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-10-18 15:06:08 -0400

    bash_src/lib.sh:index.html:main.js: onbody_fn working

--- bash_src/lib.sh
+++ bash_src/lib.sh
@@ -1,8 +1,17 @@
 LIB_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-hello_fn() {
-    echo "hello from hello_fn $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
+onwindow_fn() {
+    echo "hello from onwindow_fn $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
 }
+
+onbody_fn() {
+    echo "hello from onbody_fn   $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
+}
+
+hello_fn() {
+    echo "hello from hello_fn    $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
+}
+
 hello_fn2() {
-    echo "hello from hello_fn2 $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
+    echo "hello from hello_fn2   $(pwd) - LIB_SCRIPT_DIR $LIB_SCRIPT_DIR"
 }
